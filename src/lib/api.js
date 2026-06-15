2adb24fe3db822020d250771fd0a8b0e11360c77
oktav mac <vszeentertain@gmail.com>
2026-06-15 16:27:08 +0700

    hardcode backend url

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE = process.env.REACT_APP_BACKEND_URL || "/api";
+export const API_BASE = "https://stky.my.id/api";
 
 export const api = axios.create({
   baseURL: API_BASE,
