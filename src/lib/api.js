13df8486dca598288ef005d1990d65c7e613a651
oktav mac <vszeentertain@gmail.com>
2026-06-15 16:11:54 +0700

    use backend env url

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE = "/api";
+export const API_BASE = process.env.REACT_APP_BACKEND_URL || "/api";
 
 export const api = axios.create({
   baseURL: API_BASE,
